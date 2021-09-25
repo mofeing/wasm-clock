552ddbbe590a3cec2ee430f26ee8813888719d31
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2021-09-26 00:28:19 +0200

    Remove comented code

--- src/main.rs
+++ src/main.rs
@@ -2,45 +2,6 @@ use gloo_timers::callback::Interval;
 use js_sys::Date;
 use yew::{prelude::*, services::RenderService};
 
-// #[derive(Clone)]
-// struct TickProperties {
-//     angle: f64,
-//     stroke: u32,
-//     width: u32,
-//     height: u32,
-// }
-
-// impl Properties for TickProperties {}
-
-// struct Tick {
-//     link: ComponentLink<Self>,
-//     props: TickProperties,
-// }
-
-// impl Component for Tick {
-//     type Message = ();
-
-//     type Properties = TickProperties;
-
-//     fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
-//         Self { link, props }
-//     }
-
-//     fn update(&mut self, msg: Self::Message) -> ShouldRender {
-//         todo!()
-//     }
-
-//     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-//         todo!()
-//     }
-
-//     fn view(&self) -> Html {
-//         html! {
-//             <line x1=100 y1=100 x2=format!("{}", 100-self.props.height) y2=100 stroke=format!("{}", self.props.stroke) stroke-width=format!("{}", self.props.width) stroke-linecap="round" transform=format!("rotate({} 100 100)", self.props.angle) />
-//         }
-//     }
-// }
-
 enum Message {
     Tick,
 }
